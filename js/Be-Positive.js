4820594fc9d683def2a1406cb64f1bd58c0740a2
Jesse Copeland <jesse.copeland@gmail.com>
2015-02-21 15:28:20 -1000

    all complete except for selecting most abundent blood

---
+++ js/Be-Positive.js
@@ -0,0 +1,113 @@
+BloodType = {
+  
+  AB_POS : "AB_POS",
+  AB_NEG : "AB_NEG",
+  A_POS  : "A_POS",
+  A_NEG  : "A_NEG",
+  B_POS  : "B_POS",
+  B_NEG  : "B_NEG",
+  O_POS  : "O_POS",
+  O_NEG  : "O_NEG"
+
+};
+
+BloodTransfusionRules = {
+  
+  /**
+   * Set the simulation speed.
+   * @type {Number} : Valid values between 1 and 200
+   */
+  simulation_speed : 200,
+
+  /**
+   * returns BloodType, or false to give no BloodType
+   * 
+   * @name receive_patient
+   * @param {Bank} blood_inventory
+   * @param {Patient} patient
+   * @returns {BloodType or false}
+   *
+   * Patient properties {
+   *   gender : String, (MALE,FEMALE)
+   *   blood_type : String (BloodType)
+   * }
+   * 
+   * Bank properties {
+   *   AB_POS : Integer,
+   *   AB_NEG : Integer,
+   *   A_POS  : Integer,
+   *   A_NEG  : Integer,
+   *   B_POS  : Integer,
+   *   B_NEG  : Integer,
+   *   O_POS  : Integer,
+   *   O_NEG  : Integer
+   * }
+   * 
+   */
+
+  receive_patient : function (blood_inventory, patient) {
+    
+     // return patient.blood_type;
+    // give a random blood type to anyone
+    // very bad idea!
+    // return [BloodType
+    //   BloodType.AB_POS,
+    //   BloodType.AB_NEG,
+    //   BloodType.A_POS,
+    //   BloodType.A_NEG
+    // ][Math.floor(Math.random()*4)];
+
+    var bloodArray = [];
+
+    var reA = /^A_/;
+    var reB = /^B_/;
+    var reAB = /^AB_/;
+    var rePOS = /POS$/;
+    var reNEG = /NEG$/;
+    var pbt = patient.blood_type;  
+
+    switch (pbt) {
+
+      case "AB_POS":
+        bloodArray.push("AB_POS");
+
+      case "AB_NEG":
+        bloodArray.push("AB_NEG");
+      
+      case "A_POS":
+        if ((reA.test(pbt) || reAB.test(pbt)) && rePOS.test(pbt))
+          bloodArray.push("A_POS");
+      
+      case "A_NEG":
+        if (reA.test(pbt) || reAB.test(pbt))
+          bloodArray.push("A_NEG");
+      
+      case "B_POS":
+        if ((reB.test(pbt) || reAB.test(pbt)) && rePOS.test(pbt))
+          bloodArray.push("B_POS");
+
+      case "B_NEG":
+        if (reB.test(pbt) || reAB.test(pbt))
+          bloodArray.push("B_NEG");
+      
+      case "O_POS":
+        if (rePOS.test(pbt))
+          bloodArray.push("O_POS");
+      
+      case "O_NEG":
+          bloodArray.push("O_NEG");
+
+    }
+
+    console.log("patient:", pbt, "bloodArray:", bloodArray);
+
+    for (var i = 0; i < bloodArray.length; i++) {
+      console.log("bloodArray check:", bloodArray[i]);
+      if (blood_inventory[bloodArray[i]]) {
+        return bloodArray[i];
+      }
+    }
+    return false;
+  }
+
+};
